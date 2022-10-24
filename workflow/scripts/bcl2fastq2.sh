897806c9f0b1c85ea361392165cdf0fdbb5c1d09
Kristina Song <kristina.song@usherbrooke.ca>
2022-10-24 13:04:11 -0400

    bcl2fastq complete

---
+++ workflow/scripts/bcl2fastq2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=$JOBMAIL
+#SBATCH --time=5:00:00
+#SBATCH --output=%u.%x-%A[%a].out
+#SBATCH --error=%u.%x-%A[%a].err
+#SBATCH --mem=48000M
+#SBATCH --cpus-per-task=12
+
+### Modified from Gabrielle's script 
+
+run=../../data/samples/220923_NB502083_0194_AHVGMKBGXM
+path=$run/bcl
+sample_sheet=$path/sample_sheet.csv
+outpath=$run/fastq
+
+bcl2fastq -R $path -o $outpath --sample-sheet $sample_sheet \
+--minimum-trimmed-read-length 13 --mask-short-adapter-reads 13 \
+--no-lane-splitting -p 4 
