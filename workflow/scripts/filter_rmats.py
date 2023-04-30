b3f8ea6b52a2516b4e802c1489dadc291ff2e064
Sungeun Song <kris98@narval4.narval.calcul.quebec>
2023-04-30 08:49:17 -0400

    update rMATS filtered output format

--- workflow/scripts/filter_rmats.py
+++ workflow/scripts/filter_rmats.py
@@ -56,6 +56,13 @@ def parse_splicing(df,type):
         df = pd.concat([pos_df,neg_df],ignore_index=True)
     return df
 
+def binding_ovlp(df1,df2):
+    # bedtools intersect between binding interactions
+    bed1 = BedTool.from_dataframe(df1)
+    bed2 = BedTool.from_dataframe(df2)
+    intersection_df = bed1.intersect(bed2,s=True).to_dataframe()
+    return intersection_df
+
 def spliced_and_bound(splicing_df,event_type,binding_df):
     # bedtools intersect splicing events with binding interactions
     if 'start' not in splicing_df.columns:
@@ -63,10 +70,22 @@ def spliced_and_bound(splicing_df,event_type,binding_df):
     splicing_bed = BedTool.from_dataframe(splicing_df)
     binding_bed = BedTool.from_dataframe(binding_df)
     intersection_df = splicing_bed.intersect(binding_bed, s=True,u=True).to_dataframe()
-    intersection_df.columns = splicing_df.columns
-    intersection_df = intersection_df.sort_values(by=['IncLevelDifference','FDR'],key=abs,ascending=[False,True]).drop_duplicates(subset=['start','end'],keep='last')
+    if len(intersection_df)>0:
+        intersection_df.columns = splicing_df.columns
+        intersection_df = intersection_df.sort_values(by=['IncLevelDifference','FDR'],key=abs,ascending=[False,True]).drop_duplicates(subset=['start','end'],keep='last')
     return intersection_df
 
+def get_full_coordinates(filtered_df,org_df):
+    # Get full information on filtered splicing events
+    final_df = pd.DataFrame(columns=org_df.columns)
+    for i in range(len(filtered_df)):
+        gene = filtered_df.iloc[i]['geneSymbol']
+        fdr = filtered_df.iloc[i]['FDR']
+        lvldiff = filtered_df.iloc[i]['IncLevelDifference']
+        row = org_df[(org_df['geneSymbol']==gene)&(org_df['FDR']==fdr)&(org_df['IncLevelDifference']==lvldiff)]
+        final_df = pd.concat([final_df,row],ignore_index=True)
+    final_df.drop('score', axis=1,inplace=True)
+    return final_df
 
 # Kallisto TPM matrix
 tpm_df = pd.read_csv(tpm,sep='\t')
@@ -76,6 +95,10 @@ sno_df = pd.read_csv(sno_file,sep='\t')
 eftud2_df = pd.read_csv(eftud2_file,sep='\t')
 prpf8_df = pd.read_csv(prpf8_file,sep='\t')
 
+# Overlap of binding interactions
+sno_eftud2_df = binding_ovlp(sno_df,eftud2_df)
+sno_prpf8_df = binding_ovlp(sno_df,prpf8_df)
+sno_eftud2_prpf8_df = binding_ovlp(sno_eftud2_df,prpf8_df)
 
 # For each splicing event type, FILTER and find events bound by snoRNA and RBPs
 for event in SPLICING_EVENTS:
@@ -87,15 +110,19 @@ for event in SPLICING_EVENTS:
     filtered = filter_by_tpm(filtered,tpm_df)
 
     # Bound by snoRNA
+    print('Bound by SNORD22')
     sno_bound = spliced_and_bound(filtered,event_type,sno_df)
-    sno_bound.to_csv(os.path.join(out_dir,event_type+'_SNORD22.tsv'),sep='\t',index=None)
+    get_full_coordinates(sno_bound,filtered).to_csv(os.path.join(out_dir,event_type+'_SNORD22.tsv'),sep='\t',index=None)
     # Bound by snoRNA and EFTUD2
-    sno_eftud2_bound = spliced_and_bound(sno_bound,event_type,eftud2_df)
-    sno_eftud2_bound.to_csv(os.path.join(out_dir,event_type+'_SNORD22_EFTUD2.tsv'),sep='\t',index=None)
+    print('Bound by SNORD22 and EFTUD2')
+    sno_eftud2_bound = spliced_and_bound(filtered,event_type,sno_eftud2_df)
+    get_full_coordinates(sno_eftud2_bound,filtered).to_csv(os.path.join(out_dir,event_type+'_SNORD22_EFTUD2.tsv'),sep='\t',index=None)
     # Bound by snoRNA and PRPF8
-    sno_prpf8_bound = spliced_and_bound(sno_bound,event_type,prpf8_df)
-    sno_prpf8_bound.to_csv(os.path.join(out_dir,event_type+'_SNORD22_PRPF8.tsv'),sep='\t',index=None)
+    print('Bound by SNORD22 and PRPF8')
+    sno_prpf8_bound = spliced_and_bound(filtered,event_type,sno_prpf8_df)
+    get_full_coordinates(sno_prpf8_bound,filtered).to_csv(os.path.join(out_dir,event_type+'_SNORD22_PRPF8.tsv'),sep='\t',index=None)
     # Bound by snoRNA, EFTUD2 and PRPF8
-    sno_eftud2_prpf8_bound = spliced_and_bound(sno_eftud2_bound,event_type,prpf8_df)
-    sno_eftud2_prpf8_bound.to_csv(os.path.join(out_dir,event_type+'_SNORD22_EFTUD2_PRPF8.tsv'),sep='\t',index=None)
+    print('Bound by SNORD22, EFTUD2 and PRPF8')
+    sno_eftud2_prpf8_bound = spliced_and_bound(filtered,event_type,sno_eftud2_prpf8_df)
+    get_full_coordinates(sno_eftud2_prpf8_bound,filtered).to_csv(os.path.join(out_dir,event_type+'_SNORD22_EFTUD2_PRPF8.tsv'),sep='\t',index=None)
     print('Done')
